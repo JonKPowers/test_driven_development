bd86e018452a61e3169cd381b4341888af84e885
Jon Powers <Jon.K.Powers+git@gmail.com>
2019-01-24 08:26:25 -0500

    Redicrect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -37,16 +37,19 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        
+        # There is still a text box inviting her to add another item.
+        # She enters "Use peacock feathers to make a fly" (Edith
+        # is very methodical).
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
-        # There is still a text box inviting her to add another item.
-        # She enters "Use peacock feathers to make a fly" (Edith
-        # is very methodical).
-
-        # The page updates again and now shows both items on her list
 
         # User wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
